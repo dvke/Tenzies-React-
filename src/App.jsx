ce0c601807bea0530f34b52cf261d49da293a005
Uche Obiekwe <obiekweuchenna@gmail.com>
2023-08-04 14:53:56 +0100

    Die array of Random numbers

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,26 @@
+import Die from "./components/Die";
+import { useState } from "react";
+
 function App() {
+  const [dice, setDice] = useState(allNewDice());
+
+  function allNewDice() {
+    const randomArr = [];
+    for (let i = 0; i < 10; i++) {
+      randomArr.push(Math.floor(Math.random() * 6));
+    }
+    return randomArr;
+  }
+
+  const diceElements = dice.map((die, index) => {
+    return <Die value={die} key={index} />;
+  });
+
   return (
     <>
-      <main>Hi there!</main>
+      <main>
+        <div className="dice-container">{diceElements}</div>
+      </main>
     </>
   );
 }
---
+++ src/components/Die.jsx
@@ -0,0 +1,6 @@
+const Die = (props) => {
+  // eslint-disable-next-line react/prop-types
+  return <div className="die">{props.value}</div>;
+};
+
+export default Die;
