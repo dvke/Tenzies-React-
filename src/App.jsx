9e7cefb8cc737ad6aeb0beb207f6d0f227edeafb
Uche Obiekwe <obiekweuchenna@gmail.com>
2023-08-04 15:26:25 +0100

    Roll button and description

--- src/App.jsx
+++ src/App.jsx
@@ -16,10 +16,24 @@ function App() {
     return <Die value={die} key={index} />;
   });
 
+  function RollDice() {
+    setDice(allNewDice());
+  }
+
   return (
     <>
       <main>
+        <div className="description">
+          <h1>Tenzies</h1>
+          <p>
+            Roll until all dice are the same. Click each die to freeze it at its
+            current value between rolls.
+          </p>
+        </div>
         <div className="dice-container">{diceElements}</div>
+        <button onClick={RollDice} className="roll-btn">
+          Roll
+        </button>
       </main>
     </>
   );
